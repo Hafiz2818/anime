541af9c81ed3efa88a76662017cbb1b10e7fca9f
Hafiz <hapisnovalrianto@gmail.com>
2025-09-03 10:53:34 +0700

    fix: tambah axios dan pastikan api ter-detect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware untuk parsing URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
