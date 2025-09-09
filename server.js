b5ae0916c19c6a4be99ce746a2d400cde27a3c8d
Hafiz <hapisnovalrianto@gmail.com>
2025-09-09 10:36:48 +0700

    Update

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.BREVO_SMTP_USER, // Ambil dari env
+    user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS
   }
 });
